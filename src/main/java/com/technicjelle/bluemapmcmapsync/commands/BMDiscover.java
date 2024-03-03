d68479565e0ce4c13061d247bbaf5428c2318f90
TechnicJelle <22576047+TechnicJelle@users.noreply.github.com>
2024-03-03 21:06:03 +0100

    Reduce nesting

--- src/main/java/com/technicjelle/bluemapmcmapsync/commands/BMDiscover.java
+++ src/main/java/com/technicjelle/bluemapmcmapsync/commands/BMDiscover.java
@@ -30,58 +30,59 @@ public class BMDiscover implements CommandExecutor, TabCompleter {
 
 	@Override
 	public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, String[] args) {
-		if (sender instanceof Player) {
-			Player player = (Player) sender;
-			ItemStack heldItem = player.getInventory().getItemInMainHand();
-			ItemMeta meta = heldItem.getItemMeta();
-			if (!(meta instanceof MapMeta)) {
-				player.sendMessage(ChatColor.RED + "You must be holding a map in your main hand to sync it");
-				return true;
-			}
-			MapMeta mapMeta = (MapMeta) meta;
-			MapView mapView = mapMeta.getMapView();
-			if (mapView == null) {
-				player.sendMessage(ChatColor.RED + "This map does not exist on the server.");
-				return true;
-			}
-			World world = mapView.getWorld();
-			if (world == null) {
-				player.sendMessage(ChatColor.RED + "The world this map is associated with is not loaded.");
-				return true;
-			}
-			MapView.Scale scale = mapView.getScale();
-			int radius = getRadiusFromScale(scale);
-			if (radius == -1) {
-				player.sendMessage(ChatColor.RED + "Map's scale is not recognized.");
-				return true;
-			}
-			BlueMapAPI api = BlueMapAPI.getInstance().orElse(null);
-			if (api == null) {
-				player.sendMessage(ChatColor.RED + "BlueMap is not loaded. Try again later.");
-				return true;
-			}
-			BlueMapWorld blueMapWorld = api.getWorld(world).orElse(null);
-			if (blueMapWorld == null) {
-				player.sendMessage(ChatColor.RED + "Cannot find the BlueMapWorld of this Bukkit World.");
-				return true;
-			}
-			if (blueMapWorld.getMaps().isEmpty()) {
-				player.sendMessage(ChatColor.RED + "No maps found for this world.");
-				return true;
-			}
-
-			// Discover every BlueMap map of this world
-			for (BlueMapMap map : blueMapWorld.getMaps()) {
-				Square square = new Square(mapView.getCenterX(), mapView.getCenterZ(), radius, map);
-				if (plugin.addSquareToMap(square, map)) {
-					player.sendMessage("Discovered another piece of " + map.getName() + "!");
-				} else {
-					player.sendMessage("This part of " + map.getName() + " has already been discovered");
-				}
-			}
+		if (!(sender instanceof Player)) {
+			sender.sendMessage(ChatColor.RED + "You must be a player to use this command.");
 			return true;
 		}
-		return false;
+		Player player = (Player) sender;
+		ItemStack heldItem = player.getInventory().getItemInMainHand();
+		ItemMeta meta = heldItem.getItemMeta();
+		if (!(meta instanceof MapMeta)) {
+			player.sendMessage(ChatColor.RED + "You must be holding a map in your main hand to sync it");
+			return true;
+		}
+		MapMeta mapMeta = (MapMeta) meta;
+		MapView mapView = mapMeta.getMapView();
+		if (mapView == null) {
+			player.sendMessage(ChatColor.RED + "This map does not exist on the server.");
+			return true;
+		}
+		World world = mapView.getWorld();
+		if (world == null) {
+			player.sendMessage(ChatColor.RED + "The world this map is associated with is not loaded.");
+			return true;
+		}
+		MapView.Scale scale = mapView.getScale();
+		int radius = getRadiusFromScale(scale);
+		if (radius == -1) {
+			player.sendMessage(ChatColor.RED + "Map's scale is not recognized.");
+			return true;
+		}
+		BlueMapAPI api = BlueMapAPI.getInstance().orElse(null);
+		if (api == null) {
+			player.sendMessage(ChatColor.RED + "BlueMap is not loaded. Try again later.");
+			return true;
+		}
+		BlueMapWorld blueMapWorld = api.getWorld(world).orElse(null);
+		if (blueMapWorld == null) {
+			player.sendMessage(ChatColor.RED + "Cannot find the BlueMapWorld of this Bukkit World.");
+			return true;
+		}
+		if (blueMapWorld.getMaps().isEmpty()) {
+			player.sendMessage(ChatColor.RED + "No maps found for this world.");
+			return true;
+		}
+
+		// Discover every BlueMap map of this world
+		for (BlueMapMap map : blueMapWorld.getMaps()) {
+			Square square = new Square(mapView.getCenterX(), mapView.getCenterZ(), radius, map);
+			if (plugin.addSquareToMap(square, map)) {
+				player.sendMessage("Discovered another piece of " + map.getName() + "!");
+			} else {
+				player.sendMessage("This part of " + map.getName() + " has already been discovered");
+			}
+		}
+		return true;
 	}
 
 	/**
